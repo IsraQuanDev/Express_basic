4a376dcaee5e98f29116d0249a7bafc18de952e5
Francisco Israel Mecillas <francisco_polisol@hotmail.com>
2022-04-27 21:40:03 -0500

    Añadiendo más codigo

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const port = 3000 // localhost:3000
 app.get('/',(req,res)=> {
     res.send("Hola a todos mis explorers")
 })
+
+app.get('/launchx',(req,res)=> {
+    res.send("Bienvenidos a LaunchX")
+})
+
+
 //app.get('/url...')
 app.listen(port,()=>{
     console.log("Server Listo!")
